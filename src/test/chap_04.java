345da3fcd14c5569fdd82ac39652e10d1c81a622
yoon <sin020511@gmail.com>
2025-07-03 16:57:50 +0900

    [코테 연습] chap_04 ~ chap_06

--- src/test/chap_04.java
+++ src/test/chap_04.java
@@ -10,40 +10,42 @@ public class chap_04 {
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         sc.close();
 
         int[] temp = new int[n];
-        int count_temp = 0;
-        for(int i = 0; i < n; i++) {
-
-            boolean isHave = false;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            boolean flag = false;
             for (int j = 0; j < n; j++) {
-                if(arr[i] == temp[j]) {
-                    isHave = true;
+                if (arr[i] == temp[j])
+                {
+                    flag = true;
                     break;
                 }
             }
-            if(!isHave){
-                temp[count_temp] = arr[i];
-                count_temp++;
+            if (!flag)
+            {
+                temp[count] = arr[i];
+                count++;
             }
         }
 
-        int[] index = new int[count_temp];
+        int[] index = new int[count];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < count_temp; j++) {
-                if (arr[i] == temp[j]) {
+            for (int j = 0; j < count; j++) {
+                if (arr[i] == temp[j])
+                {
                     index[j] += 1;
                 }
             }
         }
 
-        for (int i = 0; i < count_temp; i++) {
+        for (int i = 0; i < count; i++) {
             if (index[i] > 1) {
-                for (int j = 0; j <count_temp*( count_temp-1)/2 ; j++) {
+                for (int j = 0; j < index[i] * (index[i]-1)/2; j++) {
                     System.out.println("(" + temp[i] + "," + temp[i] + ")");
                 }
             }
